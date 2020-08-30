a66e2e9fa65ebc747186011092a76f8c52a24d74
Josh Morris <jmo0624@gmail.com>
2020-08-30 16:26:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ function returnLastTwoDrivers(array) {
   return [array[array.length-2], array[array.length-1]];
 }
 
-const selctingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
